633bbc820380e27fe12e8515b6f0c283fa2666c1
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-15 18:40:34 +0300

    fix(movieslist): add paddings to movieslist

--- src/appWrapper.ts
+++ src/appWrapper.ts
@@ -6,5 +6,6 @@ export const StyledApp = styled.div`
   grid-template-columns: 300px 1fr;
   gap: 30px;
   width: 100%;
+  min-height: 100vh;
   background: ${Color.Primary_BG};
 `;
--- src/components/MoviesList/style.ts
+++ src/components/MoviesList/style.ts
@@ -6,4 +6,6 @@ export const StyledList = styled.ul`
   grid-template-rows: 1fr 1fr;
   gap: 40px;
   width: 80%;
+  height: 80%;
+  padding: 50px 0 200px 0;
 `;
