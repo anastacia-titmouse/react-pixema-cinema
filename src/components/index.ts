98e5e690bc161e1afa8929b2d6b76e641152c052
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-15 17:20:25 +0300

    feat(components): add imports of new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 import { MovieCard } from "./MovieCard/MovieCard";
 import { MoviesList } from "./MoviesList/MoviesList";
 import { Search } from "./Search/Search";
+import { SidebarLeft } from "./SidebarLeft/SidebarLeft";
+import { Nav } from "./Nav/Nav";
+import { Main } from "./Main/Main";
 
-export { MovieCard, MoviesList, Search };
+export { MovieCard, MoviesList, Search, SidebarLeft, Nav, Main };
