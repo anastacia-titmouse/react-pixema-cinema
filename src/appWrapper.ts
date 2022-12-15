52052e4d5e2356c7ef4a45258acb963e16bcb13d
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-15 18:11:33 +0300

    fix(sidebar): add paddings to sidebar

--- src/appWrapper.ts
+++ src/appWrapper.ts
@@ -4,6 +4,7 @@ import { Color } from "./ui";
 export const StyledApp = styled.div`
   display: grid;
   grid-template-columns: 300px 1fr;
+  gap: 30px;
   width: 100%;
   background: ${Color.Primary_BG};
 `;
--- src/components/SidebarLeft/style.ts
+++ src/components/SidebarLeft/style.ts
@@ -4,8 +4,10 @@ import { Color } from "../../ui";
 export const SidebarStyled = styled.aside`
   display: grid;
   grid-template-rows: 1fr 5fr 1fr;
+  padding: 48px 60px 64px 62px;
 `;
 
 export const SubTitle = styled.h6`
   color: ${Color.Light};
+  align-self: end;
 `;
