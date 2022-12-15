20aaa524bd238ec31fab4ecec63f670c2baa3443
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-15 17:18:42 +0300

    fix(styles): change dark color of theme

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -2,9 +2,9 @@ import { css } from "styled-components";
 
 export const darkTheme = css`
   --primary: #ffffff;
-  --bg-primary: #444444;
+  --bg-primary: #000000;
 `;
 export const lightTheme = css`
-  --primary: #444444;
+  --primary: #000000;
   --bg-primary: #ffffff;
 `;
