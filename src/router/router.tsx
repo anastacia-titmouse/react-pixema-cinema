70282aaa636f04466e824ac7af0f8428c6959a5f
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-17 01:23:58 +0300

    feat(router): add routers and routes

---
+++ src/router/router.tsx
@@ -0,0 +1,29 @@
+import {
+  HomePage,
+  FavoritesPage,
+  TrendsPage,
+  PasswordPage,
+  SearchPage,
+  SettingsPage,
+  SignInPage,
+  SignUpPage,
+  MovieCardPage,
+} from "pages";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { ROUTE } from "./routes";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path={ROUTE.HOME} element={<HomePage />} />
+      <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
+      <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
+      <Route path={ROUTE.PASSWORD} element={<PasswordPage />} />
+      <Route path={ROUTE.SEARCH} element={<SearchPage />} />
+      <Route path={ROUTE.SETTINGS} element={<SettingsPage />} />
+      <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
+      <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
+      <Route path={ROUTE.MOVIE_CARD} element={<MovieCardPage />} />
+    </Route>,
+  ),
+);
---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+export enum ROUTE {
+  HOME = "/",
+  TRENDS = "trends",
+  FAVORITES = "favorites",
+  SETTINGS = "settings",
+  MOVIE_CARD = "movie-card",
+  SEARCH = "search",
+  SIGN_IN = "sign-in",
+  SIGN_UP = "sign-up",
+  PASSWORD = "reset-password",
+}
