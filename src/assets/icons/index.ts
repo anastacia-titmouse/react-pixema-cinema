2592fc1b041d19acc264f5189c360abda128d4d7
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-22 19:50:07 +0300

    fix(assets): rename icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,7 @@
-import { ReactComponent as HomeIcon } from "./home_icon.svg";
-import { ReactComponent as TrendsIcon } from "./trends_icon.svg";
-import { ReactComponent as FavoritesIcon } from "./favorites_icon.svg";
-import { ReactComponent as SettingsIcon } from "./settings_icon.svg";
+import { ReactComponent as HomeIcon } from "./home.svg";
+import { ReactComponent as TrendsIcon } from "./trends.svg";
+import { ReactComponent as FavoritesIcon } from "./favorites.svg";
+import { ReactComponent as SettingsIcon } from "./settings.svg";
 import { ReactComponent as PixemaIcon } from "./pixema.svg";
 
-export {HomeIcon, TrendsIcon, FavoritesIcon, SettingsIcon, PixemaIcon};
+export { HomeIcon, TrendsIcon, FavoritesIcon, SettingsIcon, PixemaIcon };
