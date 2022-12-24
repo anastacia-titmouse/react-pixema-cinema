8e0d52e6fc04920049e8d9596ef9230829509456
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-12-25 00:14:20 +0300

    fix(styles): delete odd selectors from resetcss

--- src/ui/resetCSS.ts
+++ src/ui/resetCSS.ts
@@ -5,9 +5,6 @@ export const resetCSS = css`
   body,
   div,
   span,
-  applet,
-  object,
-  iframe,
   h1,
   h2,
   h3,
@@ -18,70 +15,18 @@ export const resetCSS = css`
   blockquote,
   pre,
   a,
-  abbr,
-  acronym,
-  address,
-  big,
-  cite,
-  code,
-  del,
-  dfn,
-  em,
   img,
-  ins,
-  kbd,
-  q,
-  s,
-  samp,
-  small,
-  strike,
-  strong,
-  sub,
-  sup,
-  tt,
-  var,
-  b,
-  u,
-  i,
-  center,
-  dl,
-  dt,
-  dd,
   ol,
   ul,
   li,
-  fieldset,
   form,
   label,
-  legend,
-  table,
-  caption,
-  tbody,
-  tfoot,
-  thead,
-  tr,
-  th,
-  td,
-  article,
   aside,
-  canvas,
-  details,
-  embed,
   figure,
   figcaption,
   footer,
   header,
-  hgroup,
-  menu,
-  nav,
-  output,
-  ruby,
-  section,
-  summary,
-  time,
-  mark,
-  audio,
-  video {
+  nav {
     margin: 0;
     padding: 0;
     border: 0;
@@ -90,7 +35,6 @@ export const resetCSS = css`
     vertical-align: baseline;
     box-sizing: border-box;
   }
-  /* HTML5 display-role reset for older browsers */
 
   body {
     line-height: 1;
@@ -102,20 +46,5 @@ export const resetCSS = css`
   }
   a {
     text-decoration: none;
-  }
-  blockquote,
-  q {
-    quotes: none;
-  }
-  blockquote:before,
-  blockquote:after,
-  q:before,
-  q:after {
-    content: "";
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
   }
 `;
