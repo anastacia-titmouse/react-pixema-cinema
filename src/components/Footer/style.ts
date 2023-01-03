0e99de68df451eeac8239457d641bbf153343cc9
anastacia-titmouse <anastacia.sinitsa@gmail.com>
2023-01-03 21:12:59 +0300

    feat(pages): add trends page
    fix(styles): fix footer styles

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import { Color } from "ui";
+import { INPUT } from "ui/typography";
 
 export const FooterStyled = styled.footer`
-  display: grid;
-  grid-template-columns: 300px 1fr;
   grid-area: footer;
-  padding: 20px 30px;
+  margin-top: auto;
 `;
 
 export const SubTitle = styled.h6`
+  text-align: center;
   color: ${Color.Light};
+  ${INPUT}
 `;
