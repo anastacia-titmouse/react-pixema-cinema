c2bae2b88c1c6e52e35d4a65116df8fdffe1d234
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2023-01-16 18:09:57 +0300

    fix(responsive): change width for mobile

--- src/ui/media.ts
+++ src/ui/media.ts
@@ -1,5 +1,5 @@
 export enum Media {
-  Mobile = "@media (max-width: 320px)",
+  Mobile = "@media (max-width: 578px)",
   Tablet = "@media (max-width: 768px)",
   Desktop_S = "@media (max-width: 1024px)",
   Desktop_M = "@media (max-width: 1280px)",
